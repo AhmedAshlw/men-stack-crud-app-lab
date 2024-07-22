79d77acb978aee67e4a2827b0342c81084bb8946
ahmedahshlw <ahmedit5216@gmail.com>
2024-07-22 15:29:52 +0300

    add create and display it

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config(); 
 const express = require("express");
+const methodOverride = require("method-override");
 const morgan = require("morgan");
 const foodsCtrl = require("./controllers/foods");
 
@@ -10,6 +11,8 @@ const app = express();
 
 //MIDDLEWARE
 app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
 const home = app.get("/", (req, res, next) => {
     res.render('index.ejs');
@@ -22,7 +25,20 @@ const home = app.get("/", (req, res, next) => {
     res.render("foods/new.ejs");
   });
 
+const findFood = app.get("/foods/:foodId", async (req, res) => {
+    const foundFood = await Food.findById(req.params.foodId);
+    res.render('foods/index.ejs', {food: foundFood});
+})
 
+const readyFood = app.post("/foods",  async (req, res, next) => {
+  await Food.create(req.body);
+  res.redirect("/foods"); 
+});
+
+const Foodidx = app.get("/foods", async (req, res) => {
+  const allFoods = await Food.find();
+  res.render("foods/index.ejs", { foods: allFoods });
+});
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
