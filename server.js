f505431653ad2f5f3404e082bfd9dc9bf437ad27
ahmedahshlw <ahmedit5216@gmail.com>
2024-07-22 21:30:35 +0300

    add edit and update and delete

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ const home = app.get("/", (req, res, next) => {
   });
 
 const findFood = app.get("/foods/:foodId", async (req, res) => {
-    const foundFood = await Food.findById(req.params.foodId);
-    res.render('foods/index.ejs', {food: foundFood});
+    const food = await Food.findById(req.params.foodId);
+    res.render('foods/show.ejs', {food});
 })
 
 const readyFood = app.post("/foods",  async (req, res, next) => {
@@ -39,6 +39,22 @@ const Foodidx = app.get("/foods", async (req, res) => {
   const allFoods = await Food.find();
   res.render("foods/index.ejs", { foods: allFoods });
 });
+
+const FoodDelete = app.delete("/foods/:foodId", async (req, res) => {
+  await Food.findByIdAndDelete(req.params.foodId);
+  res.redirect("/foods");
+});
+
+const FoodEdit = app.get("/foods/:foodId/edit", async (req, res) => {
+  const food = await Food.findById(req.params.foodId);
+  res.render("foods/edit.ejs", {food});
+});
+
+const FoodUpdate =  app.put("/foods/:foodId", async (req, res) => {
+  await Food.findByIdAndUpdate(req.params.foodId, req.body);
+  res.redirect(`/foods/${req.params.foodId}`);
+});
+
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
